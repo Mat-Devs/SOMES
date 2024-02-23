113fbf137837e8befb3776e49d208c108329c03a
Lasith Dissanayake <86358728+DissanayakeLYB@users.noreply.github.com>
2024-02-24 01:24:50 +0530

    ..

--- index.js
+++ index.js
@@ -18,3 +18,60 @@ function setImg(){
 	return slider_img.setAttribute('src', "resources/"+images[i]);
 	
 }
+
+var slider_img = document.querySelector('.slider-img');
+var dots = document.querySelectorAll('.dot');
+var images = ['1.png', '2.png', '3.png', '4.png', '5.png'];
+var currentSlideIndex = 0;
+var intervalId;
+
+function startSlideshow() {
+    intervalId = setInterval(next, 5000); // Change photo every 2 seconds (2000 milliseconds)
+}
+
+function stopSlideshow() {
+    clearInterval(intervalId);
+}
+
+function showSlide(index) {
+    slider_img.setAttribute('src', "resources/"+images[index]);
+    updateDots(index);
+}
+
+function updateDots(index) {
+    dots.forEach(dot => dot.classList.remove('active'));
+    dots[index].classList.add('active');
+}
+
+function currentSlide(index) {
+    currentSlideIndex = index - 1;
+    showSlide(currentSlideIndex);
+}
+
+function prev() {
+    currentSlideIndex--;
+    if (currentSlideIndex < 0) {
+        currentSlideIndex = images.length - 1;
+    }
+    showSlide(currentSlideIndex);
+}
+
+function next() {
+    currentSlideIndex++;
+    if (currentSlideIndex >= images.length) {
+        currentSlideIndex = 0;
+    }
+    showSlide(currentSlideIndex);
+}
+
+// Initially show the first slide
+showSlide(currentSlideIndex);
+
+// Start slideshow automatically when the page loads
+startSlideshow();
+
+// Optionally, you can stop the slideshow when the user hovers over the image
+slider_img.addEventListener('mouseover', stopSlideshow);
+
+// And resume it when the user moves the mouse out of the image
+slider_img.addEventListener('mouseout', startSlideshow);
